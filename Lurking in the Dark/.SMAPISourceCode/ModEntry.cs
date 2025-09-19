9cf3477e6221e096c4a4ccde23006650d92ec189
7thAxis <7thAxisStudios@gmail.com>
2025-09-18 23:48:18 -0700

    smapi source

---
+++ Lurking in the Dark/.SMAPISourceCode/ModEntry.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.IO;
+using System.Linq.Expressions;
+using System.Reflection;
+using System.Reflection.Metadata.Ecma335;
+using System.Xml.Linq;
+using HarmonyLib;
+using Microsoft.Xna.Framework;
+using StardewModdingAPI;
+using StardewModdingAPI.Events;
+using StardewValley;
+
+
+namespace Lurking_in_the_Dark
+{
+    public partial class ModEntry : Mod
+    {
+        public override void Entry(IModHelper helper)
+        {
+            helper.Events.GameLoop.SaveLoaded += SaveLoaded;
+            var harmony = new Harmony(ModManifest.UniqueID);
+            harmony.Patch(
+                original: AccessTools.Method(typeof(NPC), "resetForNewDay"),
+                prefix: new HarmonyMethod(typeof(Patches), nameof(Patches.resetForNewDay_Prefix))
+            );
+        }
+        public void SaveLoaded(object sender, SaveLoadedEventArgs e)
+        {
+            try
+            {
+                var Jem = Game1.getCharacterFromName("JeremyTSnail");
+                if (Jem != null)
+                    Jem.forceOneTileWide.Set(true);
+            }
+            catch
+            {
+            }
+        }
+    }
+}
